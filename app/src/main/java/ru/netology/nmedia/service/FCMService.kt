f19a298d1e3750fbe6de32dd5dbaeca0e79caa21
Aleksandr <le86ksa@gmail.com>
2025-05-13 16:57:01 +0300

    new commit + gradle

---
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -0,0 +1,15 @@
+package ru.netology.nmedia.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
